f786705434fd522f1f2d4106ba9d95cf6c0cfa28
thernparviz <atakishiyevp@gmail.com>
2024-04-30 17:31:18 +0300

    database connection

--- server/main.py
+++ server/main.py
@@ -2,7 +2,14 @@ from fastapi import FastAPI
 from socketio import AsyncServer, ASGIApp
 import random
 from datetime import datetime
+from tinydb import TinyDB, Query
 
+db = TinyDB('statistics.json')
+stats_query = Query()
+if len(db.search(stats_query.num_users.exists())) < 1:
+    db.insert({"num_users":0})
+if len(db.search(stats_query.num_messages.exists())) < 1:
+    db.insert({"num_messages":0})
 
 class User():
     def __init__(self,username,sid):
@@ -28,11 +35,20 @@ async def root():
 async def connect(sid, env):
     print("Client Connected: "+" "+str(sid))
     users[sid] = User(env['HTTP_USERNAME'],str(sid))
-    
+
+    count = db.search(stats_query.num_users.exists())[0]["num_users"]
+    db.update({"num_users": count+1},stats_query.num_users.exists())
+
+
 @sio.on("disconnect")
 async def disconnect(sid):
     print("Client Disconnected: "+" "+str(sid))
     users.pop(str(sid))
+
+    count = db.search(stats_query.num_users.exists())[0]["num_users"]
+    db.update({"num_users": count-1},stats_query.num_users.exists())
+    
+
     
 @sio.on("chat_message")
 async def chat_message(sid, data):
@@ -41,6 +57,10 @@ async def chat_message(sid, data):
     current_time = c.strftime('%H:%M:%S')
 
     await sio.emit('chat_message', [users[sid].username, data, users[sid].color,current_time] ) 
+
+    count = db.search(stats_query.num_messages.exists())[0]["num_messages"]
+    db.update({"num_messages": count +1},stats_query.num_messages.exists())
+
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run("main:app", host="127.0.0.1", port=8000)
